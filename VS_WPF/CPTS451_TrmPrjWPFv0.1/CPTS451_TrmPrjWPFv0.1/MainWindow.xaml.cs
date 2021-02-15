b8f1ac047a04ca47839f65152a0469ed2a85ee81
Zach-G <z.gherman@gmail.com>
2021-02-14 17:54:29 -0800

    Added some functions from video.
    This is the project from Sakire's video up to about 17 minutes.. The datagrid is not wanting to display the business names, states or cities associated with the columns and rows. Unsure why. Will continue to populate the project following the video some more later after I've eaten.

--- VS_WPF/CPTS451_TrmPrjWPFv0.1/CPTS451_TrmPrjWPFv0.1/MainWindow.xaml.cs
+++ VS_WPF/CPTS451_TrmPrjWPFv0.1/CPTS451_TrmPrjWPFv0.1/MainWindow.xaml.cs
@@ -20,9 +20,51 @@ namespace CPTS451_TrmPrjWPFv0._1
     /// </summary>
     public partial class MainWindow : Window
     {
+        public partial class Business
+        {
+            public string name { get; set; }
+            public string state { get; set; }
+            public string city { get; set;  }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            addState();
+        }
+
+        private void addState()
+        {
+            stateListComboBox.Items.Add("WA");
+            stateListComboBox.Items.Add("CA");
+            stateListComboBox.Items.Add("ID");
+            stateListComboBox.Items.Add("NV");
+        }
+
+        private void addColumns2Grid()
+        {
+            DataGridTextColumn col1 = new DataGridTextColumn();
+            col1.Binding = new Binding("name");
+            col1.Header = "BusinessName";
+            col1.Width = 255;
+            businessGridDataGrid.Columns.Add(col1);
+
+            DataGridTextColumn col2 = new DataGridTextColumn();
+            col2.Binding = new Binding("state");
+            col2.Header = "State";
+            col2.Width = 60;
+            businessGridDataGrid.Columns.Add(col2);
+
+            DataGridTextColumn col3 = new DataGridTextColumn();
+            col3.Binding = new Binding("city");
+            col3.Header = "City";
+            col3.Width = 150;
+            businessGridDataGrid.Columns.Add(col3);
+
+            businessGridDataGrid.Items.Add(new Business() { name = "business-1", state = "WA", city = "Pullman" });
+            businessGridDataGrid.Items.Add(new Business() { name = "business-2", state = "CA", city = "Pasadena" });
+            businessGridDataGrid.Items.Add(new Business() { name = "business-3", state = "NV", city = "Las Vegas" });
+
         }
     }
 }
