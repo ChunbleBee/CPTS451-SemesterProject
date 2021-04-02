e526dd4221fe739687b90f11515de268d5fc52c0
Zach-G <z.gherman@gmail.com>
2021-04-02 14:36:33 -0700

    GUI IS WORKING ONCE AGAIN!

--- VS_WPF/CPTS451_TrmPrjWPFv0.1/CPTS451_TrmPrjWPFv0.1/MainWindow.xaml.cs
+++ VS_WPF/CPTS451_TrmPrjWPFv0.1/CPTS451_TrmPrjWPFv0.1/MainWindow.xaml.cs
@@ -23,15 +23,17 @@ namespace CPTS451_TrmPrjWPFv0._1
     {
         public partial class Business
         {
-            public string name { get; set; }
+            public string businessname { get; set; }
             public string state { get; set; }
             public string city { get; set;  }
+            public string businessid { get; set; }
         }
 
         public MainWindow()
         {
             InitializeComponent();
             addState();
+            addColumns2Grid();
         }
 
         // not a great way of building a connection. unsafe to show user name and password.
@@ -89,7 +91,7 @@ namespace CPTS451_TrmPrjWPFv0._1
         private void addColumns2Grid()
         {
             DataGridTextColumn col1 = new DataGridTextColumn();
-            col1.Binding = new Binding("name");
+            col1.Binding = new Binding("businessname");
             col1.Header = "BusinessName";
             col1.Width = 255;
             businessGridDataGrid.Columns.Add(col1);
@@ -106,10 +108,18 @@ namespace CPTS451_TrmPrjWPFv0._1
             col3.Width = 150;
             businessGridDataGrid.Columns.Add(col3);
 
-            businessGridDataGrid.Items.Add(new Business() { name = "business-1", state = "WA", city = "Pullman" });
-            businessGridDataGrid.Items.Add(new Business() { name = "business-2", state = "CA", city = "Pasadena" });
-            businessGridDataGrid.Items.Add(new Business() { name = "business-3", state = "NV", city = "Las Vegas" });
+            // new column of width 0 and no header to hide the info from users.
+            DataGridTextColumn col4 = new DataGridTextColumn();
+            col4.Binding = new Binding("businessid");
+            col4.Header = "";
+            col4.Width = 0;
+            businessGridDataGrid.Columns.Add(col4);
 
+            //businessGridDataGrid.Items.Add(new Business() { businessname = R.GetString(0), state = R.GetString(1), city = R.GetString(2) });
+            /*businessGridDataGrid.Items.Add(new Business() { businessname = "business-1", state = "WA", city = "Pullman" });
+            businessGridDataGrid.Items.Add(new Business() { businessname = "business-2", state = "CA", city = "Pasadena" });
+            businessGridDataGrid.Items.Add(new Business() { businessname = "business-3", state = "NV", city = "Las Vegas" });
+*/
         }
 
         private void executeQuery(string sqlstr, Action<NpgsqlDataReader> myf)
@@ -156,18 +166,19 @@ namespace CPTS451_TrmPrjWPFv0._1
         private void StateListComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             cityListComboBox.Items.Clear();
-            if (cityListComboBox.SelectedIndex > -1)
+            if (cityListComboBox.SelectedIndex == -1)
             {
-               string sqlstr = "SELECT distinct city FROM business WHERE state = '" + stateListComboBox.SelectedItem.ToString() + "' ORDER BY city";
-                        //cmd.CommandText = "SELECT name, state, city FROM business WHERE state = '" + stateListComboBox.SelectedItem.ToString() + "' AND city = '" + cityListComboBox.SelectedItem.ToString() + "ORDER BY city;";
-               executeQuery(sqlstr, addCity);
+                string sqlstr = "SELECT distinct city FROM business WHERE state = '" + stateListComboBox.SelectedItem.ToString() + "' ORDER BY city";
+                //cmd.CommandText = "SELECT businessname, state, city FROM business WHERE state = '" + stateListComboBox.SelectedItem.ToString() + "' AND city = '" + cityListComboBox.SelectedItem.ToString() + "ORDER BY city;";
+                executeQuery(sqlstr, addCity);
             }
 
         }
 
+
         private void addGridRow(NpgsqlDataReader R)
         {
-            businessGridDataGrid.Items.Add(new Business() { name = R.GetString(0), state = R.GetString(1), city = R.GetString(2) });
+            businessGridDataGrid.Items.Add(new Business() { businessname = R.GetString(0), state = R.GetString(1), city = R.GetString(2), businessid = R.GetString(3) });
 
         }
 
@@ -176,9 +187,10 @@ namespace CPTS451_TrmPrjWPFv0._1
             businessGridDataGrid.Items.Clear();
             if (stateListComboBox.SelectedIndex > -1)
             {
-                string sqlstr = "SELECT name, state, city FROM business WHERE state = '" + stateListComboBox.SelectedItem.ToString() + "' AND city = '" + cityListComboBox.SelectedItem.ToString() + "' ORDER BY name;";
+                string sqlstr = "SELECT businessname, state, city, businessid FROM business WHERE state = '" + stateListComboBox.SelectedItem.ToString() + "' AND city = '" + cityListComboBox.SelectedItem.ToString() + "' ORDER BY businessname;";
                 executeQuery(sqlstr, addGridRow);
-                
+                //executeQuery(sqlstr, addColumns2Grid); // revert changes.
+
             }
         }
 
@@ -191,6 +203,22 @@ namespace CPTS451_TrmPrjWPFv0._1
         {
             //businessCatListBox.Items.Add();
         }
+
+        private void BusinessGridDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (businessGridDataGrid.SelectedIndex > -1)
+            {
+                // grab the businessid string.
+                Business B = businessGridDataGrid.Items[businessGridDataGrid.SelectedIndex] as Business;
+                if ((B.businessid != null) && (B.businessid.ToString().CompareTo("") != 0))
+                {
+                    // create new instance of business window.
+                    businessWindow businessWindow = new businessWindow(B.businessid.ToString());
+                    businessWindow.Show(); // show the new window
+                }
+            }
+        }
+
 
         //private void CityListComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         //{
