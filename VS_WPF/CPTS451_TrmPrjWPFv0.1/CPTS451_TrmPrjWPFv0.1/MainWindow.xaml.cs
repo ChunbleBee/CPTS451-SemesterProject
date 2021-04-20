53b58552032b673eb94810f1b077de31236c04a6
ChunbleBee <ChunbleBee@github.com>
2021-04-19 17:13:15 -0700

    Added column headers for FriendsGrid and FriendsTipGrids

--- VS_WPF/CPTS451_TrmPrjWPFv0.1/CPTS451_TrmPrjWPFv0.1/MainWindow.xaml.cs
+++ VS_WPF/CPTS451_TrmPrjWPFv0.1/CPTS451_TrmPrjWPFv0.1/MainWindow.xaml.cs
@@ -21,24 +21,55 @@ namespace CPTS451_TrmPrjWPFv0._1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private NpgsqlConnection Connection { get; set; }
+
         public partial class Business
         {
-            public string businessid { get; set; } // for querying businesses in a state/city
-            public string businessname { get; set; }
-            public string state { get; set; }
-            public string city { get; set;  }
+            public string BusinessID { get; set; } // for querying businesses in a state/city
+            public string BusinessName { get; set; }
+            public string State { get; set; }
+            public string City { get; set; }
+            public int ZipCode { get; set; }
+            public float Latitude { get; set; }
+            public float Longitude { get; set; }
+            public bool IsOpen { get; set; }
+            public int ReviewCount { get; set; }
+            public float StarRating { get; set; }
+            public int NumCheckIns { get; set; }
+            public int NumTips { get; set; }
         }
 
         public class User
         {
             public string ID { get; set; }
             public string Name { get; set; }
+            public float Stars { get; set; }
+            public int Fans { get; set; }
             public DateTime CreationDate { get; set; }
+            public int FunnyRates {get; set; }
+            public int CoolRates { get; set; }
+            public int UsefulRates { get; set; }
+            public int Tips { get; set; }
+            public int Likes { get; set; }
+            public float Latitude { get; set; }
+            public float Longitude { get; set; }
+        }
+        public class Tip
+        {
+            public string BusinessID { get; set; }
+            public string UserID { get; set; }
+            DateTime CreationDate { get; set; }
+            public int Likes { get; set; }
+            public string Text { get; set; }
         }
 
         public MainWindow()
         {
             InitializeComponent();
+            this.Connection = new NpgsqlConnection(GetConnectionString());
+
+            CreateFriendsColumns();
+            CreateFriendsTipsColumns();
         }
 
         // not a great way of building a connection. unsafe to show user businessname and password.
@@ -49,6 +80,52 @@ namespace CPTS451_TrmPrjWPFv0._1
             //                                       |                                              |
             //                                       v                                              v
             return "Host = localhost; Username = postgres; Database = milestone1db; password= [ENTER YOUR PASSWORD HERE]";
+        }
+
+        private void CreateFriendsColumns()
+        {
+            DataGridTextColumn fnames = new DataGridTextColumn();
+            DataGridTextColumn flikes = new DataGridTextColumn();
+            DataGridTextColumn fstars = new DataGridTextColumn();
+            DataGridTextColumn fdate = new DataGridTextColumn();
+
+            fnames.Binding = new Binding("Name");
+            flikes.Binding = new Binding("Stars");
+            fstars.Binding = new Binding("Likes");
+            fdate.Binding = new Binding("CreationDate");
+
+            fnames.Header = "Name";
+            flikes.Header = "Likes";
+            fstars.Header = "Stars";
+            fdate.Header = "Creation Date";
+
+            this.FriendsGrid.Columns.Add(fnames);
+            this.FriendsGrid.Columns.Add(flikes);
+            this.FriendsGrid.Columns.Add(fstars);
+            this.FriendsGrid.Columns.Add(fdate);
+
+            //this.FriendsGrid.Items.Add(new User() { Name = "Taiya", Stars = 5.0F, Likes = 200000000, CreationDate = DateTime.Now });
+        }
+
+        private void CreateFriendsTipsColumns()
+        {
+            DataGridTextColumn fname = new DataGridTextColumn();
+            DataGridTextColumn fbusi = new DataGridTextColumn();
+            DataGridTextColumn fcity = new DataGridTextColumn();
+            DataGridTextColumn ftext = new DataGridTextColumn();
+            DataGridTextColumn fdate = new DataGridTextColumn();
+
+            fname.Header = "Friend";
+            fbusi.Header = "Business";
+            fcity.Header = "City";
+            ftext.Header = "Review";
+            fdate.Header = "Date";
+
+            this.FriendsTipsGrid.Columns.Add(fname);
+            this.FriendsTipsGrid.Columns.Add(fbusi);
+            this.FriendsTipsGrid.Columns.Add(fcity);
+            this.FriendsTipsGrid.Columns.Add(ftext);
+            this.FriendsTipsGrid.Columns.Add(fdate);
         }
 
         private void UserNameTextBox_GotFocus(object sender, RoutedEventArgs e)
